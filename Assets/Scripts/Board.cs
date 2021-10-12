ceee4e6f4fe4f2c640380902e5f7eb54ab16ffc3
ben <ben.afr@gmail.com>
2021-10-12 20:30:47 +0300

    Adding starter files

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+  [SerializeField] int width;
+  [SerializeField] int height;
+
+  [SerializeField] Tile tilePrefab;
+
+  void Start()
+  {
+
+  }
+
+  void Update()
+  {
+
+  }
+}
