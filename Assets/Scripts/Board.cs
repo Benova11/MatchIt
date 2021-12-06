12aba6f2438e3d06c5e0941cb8f1197ac946e673
ben <ben.afr@gmail.com>
2021-12-06 16:53:16 +0200

    Added fill board logic

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -15,8 +15,9 @@ public class Board : MonoBehaviour
   void Start()
   {
     SetupTiles();
-    m_allGamePieces = new GamePiece[width,height];
+    m_allGamePieces = new GamePiece[width, height];
     SetCameraSize();
+    FillBoardRandomly();
   }
 
   void SetupTiles()
@@ -36,14 +37,50 @@ public class Board : MonoBehaviour
 
   void SetCameraSize()
   {
-    Camera.main.transform.position = new Vector3((float) (width - 1) / 2, (float) (height - 1) / 2, -10);
+    Camera.main.transform.position = new Vector3((float)(width - 1) / 2, (float)(height - 1) / 2, -10);
 
-    float aspectRatio = (float) Screen.width / (float) Screen.height;
+    float aspectRatio = (float)Screen.width / (float)Screen.height;
 
     float verticalSize = (float)height / 2 + (float)borderSize;
 
     float horizontalSize = ((float)width / 2 + (float)borderSize) / aspectRatio;
 
     Camera.main.orthographicSize = (verticalSize > horizontalSize) ? verticalSize : horizontalSize;
+  }
+
+  GameObject GetRandomGamePiece()
+  {
+    int randomIdx = Random.Range(0, gamePiecePrefabs.Length);
+    if (gamePiecePrefabs[randomIdx] == null)
+      Debug.LogWarning("BOARD:  " + randomIdx + "does not contaion a valid GamePiece prefab!");
+    return gamePiecePrefabs[randomIdx];
+  }
+
+  void PlaceGamePiece(GamePiece gamePiece, int x, int y)
+  {
+    if (gamePiece == null)
+    {
+      Debug.LogWarning("BOARD: Ivalid GamePiece!");
+      return;
+    }
+
+    gamePiece.transform.position = new Vector3(x, y, 0);
+    gamePiece.transform.rotation = Quaternion.identity;
+    gamePiece.SetCoord(x, y);
+  }
+
+  void FillBoardRandomly()
+  {
+    for (int i = 0; i < width; i++)
+    {
+      for (int j = 0; j < height; j++)
+      {
+        GameObject randomPiece = Instantiate(GetRandomGamePiece(), Vector3.zero, Quaternion.identity);
+        if (randomPiece != null)
+        {
+          PlaceGamePiece(randomPiece.GetComponent<GamePiece>(), i, j);
+        }
+      }
+    }
   }
 }
